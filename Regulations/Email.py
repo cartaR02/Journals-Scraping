3c87011244d6727a704b8464c53408aa57fc8e3c
Carter Struck <carterstruck02@gmail.com>
2025-06-12 17:10:00 -0400

    got headlines working, adding email

---
+++ Regulations/Email.py
@@ -0,0 +1,71 @@
+import smtplib
+import ssl
+import re
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from validate_email import validate_email
+
+
+
+def my_mail(from_addr, to_addr, subject, msg_txt, html_msg="", cc_addr=""):
+    smtp_server = "mail2.targetednews.com"
+    port = 587  # For starttls
+    sender_email = "kmeek@targetednews.com"
+    password = "jsfL6Hqa"
+
+    to_array = re.split("[,;]", to_addr.replace(" ", ""))
+    for e in to_array:
+        is_valid = validate_email(e)
+        if not is_valid:
+            return "Invalid Email" + str(e)
+
+    to_string = ", ".join(to_array)
+
+    cc_array = re.split("[,;]", cc_addr.replace(" ", ""))
+    for e in cc_array:
+        is_valid = validate_email(e)
+        if not is_valid:
+            return "Invalid Email" + str(e)
+
+    cc_string = ", ".join(cc_array)
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    # Try to log in to server and send email
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        # server.ehlo() # Can be omitted
+        server.starttls(context=context)  # Secure the connection
+        # server.ehlo() # Can be omitted
+        server.login(sender_email, password)
+        # TODO: Send email here
+
+        msg = MIMEMultipart("alternative")
+        msg["From"] = from_addr
+        msg["To"] = to_string
+        if len(cc_string.strip()) > 0:
+            msg["Cc"] = cc_string
+            to_array = to_array + cc_array
+
+        msg["Subject"] = subject
+        msg.attach(MIMEText(msg_txt, "plain"))
+
+        if len(html_msg.strip()) > 0:
+            msg.attach(MIMEText(html_msg, "html"))
+
+        text = msg.as_string()
+        # server.set_debuglevel(1)
+        server.sendmail(from_addr, to_array, text)
+    except Exception as e:
+        # Print any error messages to stdout
+        print(e)
+    finally:
+        server.quit()
+
+
+# does not check for production run that gets checked before even calling this function
+def email_output():
+    print("In progress")
+
+
--- Regulations/chatgpt.py
+++ Regulations/chatgpt.py
@@ -15,7 +15,9 @@ def ask_chat_gpt(PDF_Text, current_id, comments_link, filename):
     prompt = (
  # waiting on prompt
         f"""Create a 400-word news story, with a news headline on a separate line without a period above the opening of the text, from this text of a letter to a named federal agency. Use the name of that agency in the first paragraph in full only; and subsequently use a synonym or acronym. Create stand-alone paragraphs where there are direct quotes attributed to a named letter writer. Use the persons first and last name only in the first instance.
-        All docs should start with a headline and then the subsequent paragraphs.  The first paragraph starts with "WASHINGTON, {today_str} --"
+          All docs should start with a headline a line break and then the body paragraphs.  The first body paragraph starts with "WASHINGTON, {today_str} --"
+          Headline should be based on the text and in title case
+          DO not under any circumstances return an answer without beginning with a title and then the body paragraphs without the rules stated above.
         If the agency is a department, use U.S. in front of it instead of United States spelled out.
 If there are mutiple signers for the letter, create a paragraph that lists all of them.
 If using a person's title after their name, the letters are lowercase.
@@ -28,10 +30,9 @@ The last paragraph should only say when the letter was sent to the government ag
             {"role": "system", "content": prompt},
             {"role": "user", "content": PDF_Text}])
         msg = response.choices[0].message.content
-        split_body = msg.split("\n")
+        split_body = msg.split("\n", 1)
         headline = split_body[0]
         msg = split_body[1] + "\n\n-----------------------------------\n\nView Original Submission: " + comments_link
-        headline = 'THis IS a test'
         ## TODO ADD DOCKET END OF TEXT
 
         database_saving.insert_into_db(headline, msg, PDF_Text, filename)
