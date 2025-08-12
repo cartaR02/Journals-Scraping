0aceecc805f94e833c67f2cc005d976d0da915dd
Carter Struck <carterstruck02@gmail.com>
2025-08-12 12:09:44 -0400

    fixed sites not having right tags nad bypass cloudflare

--- Journals/configs/template_csv_data.py
+++ Journals/configs/template_csv_data.py
@@ -1,8 +1,8 @@
 # journal_id,  Journal Name,    url_field,    journal_containers,     landing_page_gathering,   link_data,    headline_data,    headline_formatting_data,   date_data,    date_formatting_data,     journal_data_info,    journal_info_formatting, journal_articles, phrase_tag, abstract,    load_time,    bypass,   status
 # article tag is the "type" of article it is whether is says Research, Article, but to filter out ones that say "Introduction" Correction ,Errata etc...
 psycnet_line = ",,,find|class|volume-wrapper~find_all|elem|li,True,find|elem|a,find|elem|a,,find_all|elem|span|1|2,swap,find|class|list,,5,True,D"
-sagepub_line = ",,,find|class|loi__issues~find_all|class|loi__issue,True,find|elem|a,find|elem|a,,find|class|loi__issue__cover-date,,find|class|table-of-content,,find_all|class|issue-item__container ,find|class|issue-item__header~find_all|elem|span|2,find|elem|a,find_all|class|core-container|3~find|elem|div,5,,D"
-lww_line = ",,,find|class|loi__issues~find_all|class|loi__issue,True,find|elem|a,find|elem|a,,find|class|loi__issue__cover-date,,find|class|table-of-content,,find_all|class|issue-item__container ,find|class|issue-item__header~find_all|elem|span|2,find|elem|a,find_all|class|core-container|3~find|elem|div,5,,D"
+sagepub_line = ",,,find|class|loi__issues~find_all|class|loi__issue,True,find|elem|a,find|elem|a,,find|class|loi__issue__cover-date,,find|class|table-of-content,,find_all|class|issue-item__container ,find|class|issue-item__header~find_all|elem|span|2,find|elem|a,find_all|class|core-container|3~find|elem|div,5,True,D"
+lww_line = ",,,find|class|AVIssueList~find_all|class|AVIssueLink,True,find|elem|a,find|elem|a,,find|elem|a,,find|class|table-of-content,,find_all|class|issue-item__container ,find|class|issue-item__header~find_all|elem|span|2,find|elem|a,find_all|class|core-container|3~find|elem|div,5,,D"
 
 # saving this
 # 30179,Journal of Educational Psychology,https://psycnet.apa.org/PsycARTICLES/journal/edu/117/4|https://psycnet.apa.org
